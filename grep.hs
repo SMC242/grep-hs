60824296f3751c4cb70597790ee955c3a168d62e
SMC242 <56912943+SMC242@users.noreply.github.com>
2023-01-19 16:22:52 +0000

    Now using regex

--- grep.hs
+++ grep.hs
@@ -2,15 +2,16 @@ import Data.ByteString qualified as B
 import Data.ByteString.UTF8 qualified as BLU -- https://stackoverflow.com/questions/3232074/what-is-the-best-way-to-convert-string-to-bytestring
 import Data.List (isInfixOf)
 import Data.Maybe (catMaybes)
+import Text.Regex.TDFA
 
 data FileTree = File {name :: String, contents :: BLU.ByteString} | Directory {name :: String, children :: [FileTree]} deriving (Show)
 
-grep :: BLU.ByteString -> FileTree -> [String]
-grep x fs = catMaybes $ inner [] fs
+grep :: Regex -> FileTree -> [String]
+grep expr fs = catMaybes $ inner [] fs
   where
     inner :: [Maybe String] -> FileTree -> [Maybe String]
     inner acc f = case f of
-      File name contents -> (: acc) $ if x `B.isInfixOf` contents then Just name else Nothing
+      File name contents -> (: acc) $ if matchTest expr contents then Just name else Nothing
       Directory name children -> concatMap (inner acc) children ++ acc
 
 testTree :: FileTree
@@ -23,8 +24,15 @@ testTree =
         "Gonks"
         [ File "Original" (BLU.fromString "The first gonk; The progenitor"),
           File "My favourite" (BLU.fromString "Goth gonk <3")
-        ]
+        ],
+      File "Top 10 Gaming moments" (BLU.fromString "10: Frotnite")
     ]
 
+-- NOTE: Does not support flags
+toRegex :: String -> Regex
+toRegex = makeRegex
+
 main :: IO ()
-main = print $ grep (BLU.fromString "gonk") testTree
+main = do
+  expr <- getLine
+  print $ grep (toRegex expr) testTree
