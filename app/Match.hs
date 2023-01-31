533d014bb2aaa67170bd2c8019d2987c06571755
SMC242 <56912943+SMC242@users.noreply.github.com>
2023-01-31 16:48:47 +0000

    Keeping the MatchLength

--- app/Match.hs
+++ app/Match.hs
@@ -1,27 +1,30 @@
-module Match (matchTree, Match, toRegex) where
+module Match (matchTree, Match, toRegex, fileMatches) where
 
-import Data.Array ((!))
+import Data.Array (elems, (!))
 import Data.Maybe (catMaybes)
 import qualified Data.Text as T
-import FileTree (FileTree)
+import FileTree (FileTree, contentTree, flattenWith)
 import Text.Regex.TDFA
 
-type Match = (T.Text, MatchOffset)
+type Match = (T.Text, MatchOffset, MatchLength)
 
 -- NOTE: Using options: extended regex, case sensitive, multiline
 toRegex :: String -> Regex
 toRegex = makeRegex
 
 toMatch :: [MatchText T.Text] -> [Match]
-toMatch = map ((\(text, (offset, _)) -> (text, offset)) . (! 0))
+toMatch = map ((\(text, (offset, length)) -> (text, offset, length)) . (! 0))
 
 fileMatches :: Regex -> T.Text -> Maybe [Match]
 fileMatches expr fileContents = case matchAllText expr fileContents of
   [] -> Nothing
   matches -> Just (toMatch matches)
 
-matchTree :: Regex -> FileTree -> [String]
-matchTree expr tree = undefined
+matchTree :: Regex -> FileTree -> IO [Match]
+matchTree expr tree = do
+  let fileContents = contentTree tree
+
+  return undefined
 
 -- matchTree :: Regex -> FileTree -> [String]
 -- matchTree expr fs = catMaybes $ inner [] fs
