c30afa74b1c4f932d320ebd98ea0cac8c4c0926e
SMC242 <56912943+SMC242@users.noreply.github.com>
2023-02-02 12:42:33 +0000

    WIP: CLI

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 
+import Console.Options
 import Control.Monad
+import Data.Functor ((<&>))
 import FileTree (FileTree, contentTree, prettyTree, readDirectory)
 import Formatting (formatMatch)
 import Match (FileMatch, RegexMatch, matchTree, matches, toRegex)
-import Text.Regex.TDFA (Regex, RegexMaker (makeRegex))
+import Text.Regex.TDFA (Regex, RegexMaker (makeRegex, makeRegexM))
 
 {-
   Structure:
@@ -21,6 +23,25 @@ grep = undefined
 
 main :: IO ()
 main = do
-  let expr = makeRegex "Gonk"
-  matches <- readDirectory "../test" >>= matchTree expr
-  putStr . unlines $ map formatMatch matches
+  inputRegex
+    ( \expr -> do
+        matches <- readDirectory "../test" >>= matchTree expr
+        putStr . unlines $ map formatMatch matches
+    )
+
+readRegex :: String -> Maybe Regex
+readRegex = makeRegexM
+
+inputRegex :: (Regex -> IO ()) -> IO ()
+inputRegex continue = do
+  expr <- getLine
+  forM_ (readRegex expr) continue
+
+-- main' :: IO ()
+-- main' = defaultMain $ do
+--   programName "grep-hs"
+--   programDescription "Clone of grep(1)"
+--   flagIgnore <- flag $ FlagShort "i" <> FlagLong "ignore-case"
+--   flagMultiline <- flag $ FlagShort "m" <> FlagLong "multiline"
+--   rest <- remainingArguments "FILE"
+--   action $ \toParam -> do
